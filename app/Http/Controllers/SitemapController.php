eeee385abafa623e8a6f1c34861bf3e787eb147a
DESKTOP-I78NG2K\tatat <tatataabcd@gmail.com>
2021-10-21 22:00:38 +0900

    add sitemap

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -14,14 +14,53 @@ class SitemapController extends Controller
     {
         $sitemap = App::make("sitemap");
         // キャッシュの設定。単位は分
-        $sitemap->setCache('laravel.sitemap-index', 3600);
+        $sitemap->setCache('laravel.sitemap-basics', 3600);
         if (!$sitemap->isCached()) {
-            // sitemapのURLを追加
-            $sitemap->addSitemap(URL::route('sitemap-basics'));
-            // sitemapを増やす場合はココに追記していく。
+
+            $sitemap->add(
+                route('top'),
+                Carbon::now(),
+                1.0,
+                'weekly'
+            );
+
+            $sitemap->add(
+                route('greeting'),
+                Carbon::now(),
+                1.0,
+                'weekly'
+            );
+
+            $sitemap->add(
+                route('access'),
+                Carbon::now(),
+                1.0,
+                'weekly'
+            );
+
+            $sitemap->add(
+                route('fee'),
+                Carbon::now(),
+                1.0,
+                'weekly'
+            );
+
+            $sitemap->add(
+                route('introduction'),
+                Carbon::now(),
+                1.0,
+                'weekly'
+            );
+
+            $sitemap->add(
+                route('reservationTop'),
+                Carbon::now(),
+                1.0,
+                'weekly'
+            );
         }
         // XML形式で出力
-        return $sitemap->render('sitemapindex');
+        return $sitemap->store('xml', 'mysitemap');
     }
 
     // sitemapを出力する
@@ -75,6 +114,6 @@ class SitemapController extends Controller
             );
         }
         // XML形式で出力
-        return $sitemap->render('xml');
+        return $sitemap->store('xml', 'mysitemap');
     }
 }
