4e8e46448819570401ddfd92a9aa17ee16fe71cf
DESKTOP-I78NG2K\tatat <tatataabcd@gmail.com>
2021-10-21 22:47:25 +0900

    add sitemap

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -12,53 +12,6 @@ class SitemapController extends Controller
     // sitemap-indexを出力する
     public function index()
     {
-        $sitemap = App::make("sitemap");
-        $now = Carbon::now();
-        $sitemap->add(URL::to('/'), $now, '1.0', 'daily');
-
-        // $sitemap->add(
-        //     route('top'),
-        //     Carbon::now(),
-        //     1.0,
-        //     'weekly'
-        // );
-
-        // $sitemap->add(
-        //     route('greeting'),
-        //     Carbon::now(),
-        //     1.0,
-        //     'weekly'
-        // );
-
-        // $sitemap->add(
-        //     route('access'),
-        //     Carbon::now(),
-        //     1.0,
-        //     'weekly'
-        // );
-
-        // $sitemap->add(
-        //     route('fee'),
-        //     Carbon::now(),
-        //     1.0,
-        //     'weekly'
-        // );
-
-        // $sitemap->add(
-        //     route('introduction'),
-        //     Carbon::now(),
-        //     1.0,
-        //     'weekly'
-        // );
-
-        // $sitemap->add(
-        //     route('reservationTop'),
-        //     Carbon::now(),
-        //     1.0,
-        //     'weekly'
-        // );
-
-        // XML形式で出力
-        return $sitemap->render('xml');
+        return response()->view('sitemap')->header('Content-Type', 'text/xml');
     }
 }
