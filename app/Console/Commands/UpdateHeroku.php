4847f23e7d41d79e7e5566e05ed43ae955c63d8f
DESKTOP-I78NG2K\tatat <tatataabcd@gmail.com>
2022-02-14 13:11:34 +0900

    heroku更新コマンド作成

---
+++ app/Console/Commands/UpdateHeroku.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Http\Controllers\LineMessengerController;
+
+class UpdateHeroku extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'heroku:update';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '30分周期のスリープを回避するため、定期的にPGを動かす';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+    }
+}
