273c28d1b3a38f0ac08f14466f537d7d1509aed1
DESKTOP-I78NG2K\tatat <tatataabcd@gmail.com>
2022-03-10 00:36:59 +0900

    public register.js

--- public/js/register.js
+++ public/js/register.js
@@ -7,13 +7,13 @@ var __webpack_exports__ = {};
 // ※兄弟児がいる場合のみ入力する項目が対象
 showedOrHidedTargetElementList = [$('#age2'), $('#gender2'), $('#diagnosis2')];
 $(function () {
-  showOrHideTargetElementList($('#childName2').val(), showedOrHidedTargetElementList);
+  onHandleChangeChildName2($('#childName2').val(), showedOrHidedTargetElementList);
 });
 $('#childName2').change(function () {
-  showOrHideTargetElementList($(this).val(), showedOrHidedTargetElementList);
+  onHandleChangeChildName2($(this).val(), showedOrHidedTargetElementList);
 }); // 発火要素値の有無により、対象要素リストの表示・非表示を制御
 
-function showOrHideTargetElementList(targetEventElementVal, targetElementList) {
+function onHandleChangeChildName2(targetEventElementVal, targetElementList) {
   if (targetEventElementVal) {
     for (var i = 0; i < targetElementList.length; i++) {
       targetElementList[i].removeClass('d-none');
@@ -21,10 +21,15 @@ function showOrHideTargetElementList(targetEventElementVal, targetElementList) {
   } else {
     for (var _i = 0; _i < targetElementList.length; _i++) {
       targetElementList[_i].addClass('d-none');
+    } // 兄弟児氏名の削除時、他兄弟児要素の値を空に設定
+
+
+    targetInputElementList = [$('input[name="age2"]'), $('input[name="diagnosis2"]')];
+
+    for (var _i2 = 0; _i2 < targetInputElementList.length; _i2++) {
+      targetInputElementList[_i2].val('');
     }
   }
-} // $('#jsRegistrationBtn').click(function() {
-//     $(this).prop("disabled", true);
-// });
+}
 /******/ })()
 ;
