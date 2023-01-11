5c4a7fdc1a66bc335a22c51986266ca555b0c3e6
DESKTOP-I78NG2K\tatat <tatataabcd@gmail.com>
2023-01-11 23:43:10 +0900

    test reservationService existsDuplicateReservation

--- tests/Unit/Services/ReservationServiceTest.php
+++ tests/Unit/Services/ReservationServiceTest.php
@@ -4,20 +4,43 @@ namespace Tests\Unit\Services;
 
 use Tests\TestCase;
 use App\Models\Reservation;
+use App\Services\MailService;
 use App\Services\ReservationService;
-use Mockery;
-use Mockery\LegacyMockInterface;
-use Mockery\MockInterface;
+use App\Services\GoogleCalendarService;
+use App\Services\LineMessengerServices;
+use App\Repositories\ReservationRepository;
+use App\Services\AvailableReservationDatetimeService;
+use App\Repositories\AvailableReservationDatetimeRepository;
 
 class ReservationServiceTest extends TestCase
 {
-    private ReservationService $service;
+    private ReservationService $reservationService;
+    private GoogleCalendarService $googleCalendarService;
+    private MailService $mailService; 
+    private LineMessengerServices $lineMessengerServices;
+    private AvailableReservationDatetimeService $availableReservationDatetimeService;
+    private ReservationRepository $reservationRepository;
+    private AvailableReservationDatetimeRepository $availableReservationDatetimeRepository;
 
     public function setUp(): void
     {
         parent::setUp();
 
-        $this->service = new ReservationService();
+        $this->googleCalendarService = new GoogleCalendarService();
+        $this->mailService = new MailService();
+        $this->lineMessengerServices = new LineMessengerServices();
+        $this->availableReservationDatetimeService = new AvailableReservationDatetimeService();
+        $this->reservationRepository = new ReservationRepository();
+        $this->availableReservationDatetimeRepository = new AvailableReservationDatetimeRepository();
+
+        $this->reservationService = new ReservationService(
+            $this->googleCalendarService,
+            $this->mailService,
+            $this->lineMessengerServices,
+            $this->availableReservationDatetimeService,
+            $this->reservationRepository,
+            $this->availableReservationDatetimeRepository
+        );
     }
 
     public function testCreateReservation(): void
@@ -29,10 +52,19 @@ class ReservationServiceTest extends TestCase
 
     public function testExistsDuplicateReservation(): void
     {
+        $date = '2023/1/1';
+        $time = '11:00:00';
+
         Reservation::factory(2)->create([
-            'reservation_date' => '2023/1/1',
-            'reservation_time' => '11:00:00', 
+            'reservation_date' => $date,
+            'reservation_time' => $time, 
         ]);
-        
+
+        $isDuplicate = $this->reservationService->existsDuplicateReservation((object) [
+            'avaDate' => $date,
+            'avaTime' => $time,
+        ]);
+
+        self::assertTrue($isDuplicate);
     }
 }
