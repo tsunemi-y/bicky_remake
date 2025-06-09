e7544ca641d92275e41aeeffee6d41913377c5db
AzureAD\spos大阪常深裕 <tsunemi-yu@shiftplus.co.jp>
2025-06-09 17:29:00 +0900

    サービステスト全部作成（詳細はまだ）

---
+++ tests/Unit/Services/AvailableReservationDatetimeServiceTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Unit\Services;
+
+use Tests\TestCase;
+use App\Services\AvailableReservationDatetimeService;
+use Mockery;
+
+class AvailableReservationDatetimeServiceTest extends TestCase
+{
+    public function test_get_available_datetimes()
+    {
+        $this->markTestIncomplete('AvailableReservationDatetimeServiceのgetAvailableDatetimesテストを実装してください');
+    }
+} 
---
+++ tests/Unit/Services/GoogleCalendarServiceTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Unit\Services;
+
+use Tests\TestCase;
+use App\Services\GoogleCalendarService;
+use Mockery;
+
+class GoogleCalendarServiceTest extends TestCase
+{
+    public function test_create_event()
+    {
+        $this->markTestIncomplete('GoogleCalendarServiceのcreateEventテストを実装してください');
+    }
+} 
