89195afd8d37c6dfa0931317249fb040d94dfe1e
Yu <tatataabcd@gmail.com>
2025-06-17 23:39:11 +0900

    user seeder　作成

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -16,11 +17,14 @@ class UsersTableSeeder extends Seeder
     {
         User::create([
             'parentName' => '松岡親太郎',
-            'email' => 'tatataabcd@gmail.com',
+            'email' => 'tsunemi.devlop@gmail.com',
             'tel' => '08099999999',
-            'password' => \Hash::make('tarou0208'),
+            'password' => Hash::make('tarou0208'),
             'address' => '大阪',
             'introduction' => '区役所',
+            'consaltation' => 'サンプル相談内容',
+            'fee' => '10000',
+            'userAgent' => 'Mozilla/5.0 Sample',
         ]);
     }
 }
