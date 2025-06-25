cb5792d209a8f0444a7179b24ba4a8f07fbd8a07
Yu <tatataabcd@gmail.com>
2025-06-26 04:57:11 +0900

    http

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (App::environment('production')) {
-            URL::forceScheme('https');
+            URL::forceScheme('http');
         }
     }
 }
